7c9edea2eec314ce9cc469b44f1828f2d6f0c555
chutianwen <tianwen.chu@fedcentric.com>
2019-06-11 18:17:42 -0700

    'v3'

--- src/app/carpool-map/carpool-map.component.ts
+++ src/app/carpool-map/carpool-map.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {UsersService} from '../users.service';
 import {UserWithLoc} from '../userwithloc';
 import {User} from '../user';
 import {GeocodeService} from '../geocode.service';
 import {Observable} from 'rxjs/index';
 
-const userExample: User = {
-  userName: "Amy",
-  address: "Fairfax Blvd, Fairfax, VA",
-}
+// const userExample: User = {
+//   userName: "Amy",
+//   address: "Fairfax Blvd, Fairfax, VA",
+// }
 
-const addressExample = "Germantown Rd, Germantown, MD";
+const addressExample = 'Germantown Rd, Germantown, MD';
+
 @Component({
   selector: 'app-carpool-map',
   templateUrl: './carpool-map.component.html',
@@ -18,17 +19,30 @@ const addressExample = "Germantown Rd, Germantown, MD";
 })
 export class CarpoolMapComponent implements OnInit {
 
-  usersWithLoc: UserWithLoc[];
+  usersWithLoc: UserWithLoc[] = [];
+
   constructor(private userService: UsersService,
-               private geoCodeService: GeocodeService) { }
+              private geoCodeService: GeocodeService) {
+  }
 
   ngOnInit() {
-    this.geoCodeService.geocodeAddress(addressExample).subscribe(loc => console.log(addressExample + ' ' + loc.lat));
-    this.geoCodeService.geocodeUser(userExample).subscribe(loc => console.log(userExample.address + ' ' + loc.loc.lng));
-    this.usersWithLoc = [];
+    // alert(`usersWithLoc: ${this.usersWithLoc.length}`);
+    // this.userService.getUsers().subscribe(users => this.geoCodeService.geoCodeUserSync(users, this.usersWithLoc));
+    // this.geoCodeService.geocodeAddress(addressExample).subscribe(loc => console.log(`addressExample ${loc.lat}`));
+    // this.geoCodeService.geocodeUser(userExample).subscribe(loc => console.log(`${userExample.address} ${loc.loc.lng}`));
+
     this.userService.getUsers().subscribe((users: User[]) => {
+      console.log(`Total users: ${users.length}`);
       this.addLocToUsers(users).map((userWithLoc) => {
-        userWithLoc.subscribe(item => this.usersWithLoc.push(item));
+        userWithLoc.subscribe((item) => {
+            console.log(`Get item ${item.user}`);
+            this.usersWithLoc.push(item);
+          }, (msg) => {
+            console.log('Shit');
+          },
+          () => {
+            console.log(`wholy complete, usersWithLoc: ${this.usersWithLoc.length}`);
+          });
       });
     });
   }
--- src/app/geocode.service.ts
+++ src/app/geocode.service.ts
@@ -1,21 +1,90 @@
-import { Injectable } from '@angular/core';
-import { MapsAPILoader } from '@agm/core';
-import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
-import { of } from 'rxjs/observable/of';
-import { filter, catchError, tap, map, switchMap } from 'rxjs/operators';
-import { fromPromise } from 'rxjs/observable/fromPromise';
-import { Location } from './location-model';
-import {User} from 'user';
+import {Injectable} from '@angular/core';
+import {MapsAPILoader} from '@agm/core';
+import {Observable} from 'rxjs/Observable';
+import {of} from 'rxjs/observable/of';
+import {map, switchMap, tap} from 'rxjs/operators';
+import {fromPromise} from 'rxjs/observable/fromPromise';
+import {Location} from './location-model';
+import {User} from './user';
 import {UserWithLoc} from './userwithloc';
+import {Subscriber} from 'rxjs/index';
 
 declare var google: any;
 
-@Injectable({ providedIn: `root` })
+@Injectable({providedIn: `root`})
 export class GeocodeService {
   private geocoder: any;
 
-  constructor(private mapLoader: MapsAPILoader) {}
+  constructor(private mapLoader: MapsAPILoader) {
+  }
+
+  geocodeUser(user: User): Observable<UserWithLoc> {
+    console.log('Start geocoding!');
+    return this.waitForMapsToLoad().pipe(
+      // filter(loaded => loaded),
+      switchMap(() => {
+        return new Observable<UserWithLoc>(observer => this.helper(user, observer));
+      })
+    );
+  }
+
+  helper(user: User, observer: Subscriber<UserWithLoc>) {
+    this.geocoder.geocode({'address': user.address}, (results, status) => {
+      if (status === google.maps.GeocoderStatus.OK) {
+        console.log(`Geocoding complete: ${user.address}, ${results[0].geometry.location.lat()}`);
+        observer.next({
+          user: user,
+          loc: {
+            lat: results[0].geometry.location.lat(),
+            lng: results[0].geometry.location.lng()
+          }
+        });
+      } else if (status == google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
+        console.log(`over_query_limit: ${user.address}`);
+        setTimeout(() => this.helper(user, observer), 2000);
+      } else {
+        console.log('Error - ', results, ' & Status - ', status);
+        observer.next({
+          user: user,
+          loc: {
+            lat: 0,
+            lng: 0
+          }
+        });
+      }
+      // Need to figure out the reasoning behind. If turn it on, then usersWithLoc will have problem
+      // observer.complete();
+    });
+  }
+
+  geocodeAddress(address: String): Observable<Location> {
+    console.log('Start geocoding!');
+    return this.waitForMapsToLoad().pipe(
+      // filter(loaded => loaded),
+      switchMap(() => {
+        return new Observable<Location>(observer => {
+          this.geocoder.geocode({'address': address}, (results, status) => {
+            if (status == google.maps.GeocoderStatus.OK) {
+              console.log('Geocoding complete!');
+              observer.next(
+                {
+                  lat: results[0].geometry.location.lat(),
+                  lng: results[0].geometry.location.lng()
+                });
+            } else {
+              console.log('Error - ', results, ' & Status - ', status);
+              observer.next(
+                {
+                  lat: 0,
+                  lng: 0
+                });
+            }
+            observer.complete();
+          });
+        });
+      })
+    );
+  }
 
   private initGeocoder() {
     console.log('Init geocoder!');
@@ -33,70 +102,5 @@ export class GeocodeService {
     return of(true);
   }
 
-  geocodeUser(user: User): Observable<UserWithLoc> {
-    console.log('Start geocoding!');
-    return this.waitForMapsToLoad().pipe(
-      // filter(loaded => loaded),
-      switchMap(() => {
-        return new Observable(observer => {
-          this.geocoder.geocode({'address': user.address}, (results, status) => {
-            if (status == google.maps.GeocoderStatus.OK) {
-              console.log('Geocoding complete!');
-              observer.next({
-                user: user,
-                loc: {
-                  lat: results[0].geometry.location.lat(),
-                  lng: results[0].geometry.location.lng()
-                }
-              });
-            } else if(status == google.maps.GeocoderStatus.OVER_QUERY_LIMIT){
-              console.log("over_query_limit" + ' ' + user.address);
-              setTimeout("wait = true", 10);
-              return this.geocodeUser(user);
-            } else {
-              console.log('Error - ', results, ' & Status - ', status);
-              observer.next({
-                user: user,
-                loc: {
-                  lat: 0,
-                  lng: 0
-                }
-              });
-            }
-            observer.complete();
-          });
-        });
-      })
-    );
-  }
-
-  geocodeAddress(address: String): Observable<Location> {
-    console.log('Start geocoding!');
-    return this.waitForMapsToLoad().pipe(
-      // filter(loaded => loaded),
-      switchMap(() => {
-        return new Observable(observer => {
-          this.geocoder.geocode({'address': address}, (results, status) => {
-            if (status == google.maps.GeocoderStatus.OK) {
-              console.log('Geocoding complete!');
-              observer.next(
-                {
-                  lat: results[0].geometry.location.lat(),
-                  lng: results[0].geometry.location.lng()
-                 });
-            } else {
-              console.log('Error - ', results, ' & Status - ', status);
-              observer.next(
-                {
-                  lat: 0,
-                  lng: 0
-                });
-            }
-            observer.complete();
-          });
-        });
-      })
-    );
-  }
 
 }
